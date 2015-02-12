06d4dc2c9555a9ee06ed7b853f6fb8c7f33f1319
jacobfriis <jacob.friis@simplecomplex.net>
2015-02-12 19:26:42 +0100

    Moved css and js dirs back; out of the src dir.

--- examples/unextended/.inspect_example_unextended.php
+++ examples/unextended/.inspect_example_unextended.php
@@ -24,11 +24,11 @@ use SimpleComplex\Inspect\Inspect;
 $html = array(
   'head' => '<!DOCTYPE html>
 <html lang="en" dir="ltr">
-<link type="text/css" rel="stylesheet" href="/lib/vendor/SimpleComplex/inspect/src/css/inspect_format_output.css" media="all" />
+<link type="text/css" rel="stylesheet" href="/lib/vendor/SimpleComplex/inspect/css/inspect_format_output.css" media="all" />
 <!-- Include jQuery to get expansible/collapsible object/array listings. -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
-<script src="/lib/vendor/SimpleComplex/inspect/src/js/inspect.js"></script>
-<script src="/lib/vendor/SimpleComplex/inspect/src/js/inspect_format_output.js"></script>
+<script src="/lib/vendor/SimpleComplex/inspect/js/inspect.js"></script>
+<script src="/lib/vendor/SimpleComplex/inspect/js/inspect_format_output.js"></script>
 <head>
 <meta charset="utf-8" />
 <title>Inspect example unextended</title>
