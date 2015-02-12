6c86bc2d7353179ee6a11a200ef69b149ca895ab
jacobfriis <jacob.friis@simplecomplex.net>
2015-02-12 18:48:51 +0100

    consumer composer.json example

--- examples/unextended/.inspect_example_unextended.php
+++ examples/unextended/.inspect_example_unextended.php
@@ -5,24 +5,22 @@
 
 /*
  * INSTRUCTIONS
- * 1) Create 'lib' directory in document root.
- * 2) In commandline, execute: composer install
- * 3) Copy this file to the site's document root dir, and rename it removing the leading dot.
- * 4) Go to http://yoursite/inspect_example_unextended.php
- * 5) Remove the copied file from document root when done testing.
+ * 1) Copy this file to the site's document root dir, and rename it removing the leading dot.
+ * 2) Go to http://yoursite/inspect_example_unextended.php
+ * 3) Remove the copied file from document root when done testing.
  */
 
 // Get all errors.
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
-include 'lib/vendor/SimpleComplex/inspect/src/Inspect.php';
+// Include SimpleComplex\Inspect\Inspect.php via Composer's autoloader,
+// or include it 'manually'.
+require 'lib/vendor/autoload.php';
+//include 'lib/vendor/SimpleComplex/inspect/src/Inspect.php';
 
 use SimpleComplex\Inspect\Inspect;
 
-// Inspect globals to screen, filtered for the _SERVERS array,
-// because the existance of this script might be forgotten,
-// and _SERVERS may well reveal sensitive environment information.
 $html = array(
   'head' => '<!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -35,14 +33,34 @@ $html = array(
 <meta charset="utf-8" />
 <title>Inspect example unextended</title>
 </head>
-<body>',
+<body>
+<h3>Example of using Inspect directly, and getting the output to screen</h3>
+<h4>Extend to accommodate to the context</h4>
+<p></p>
+<h4>Output targets \'log\', \'file\', \'frontend log\' and \'get\'</h4>
+<p>\'get\'ting should be illegal in production</p>
+
+<p>
+',
   'foot' => '
 </body>'
 );
 
 echo $html['head'];
 
+// Inspect globals to screen, filtered for the _SERVERS array,
+// because the existance of this script might be forgotten,
+// and _SERVERS may well reveal sensitive environment information.
 $dateTime = new DateTime();
 echo Inspect::get($GLOBALS, array('message' => 'globals', 'filter' => '_SERVER'));
 
+// Trace an exception, to screen.
+try {
+  $date = new DateTime('Obviously not a parsable date');
+}
+catch (Exception $xc) {
+  echo Inspect::traceGet($xc, 'Stupid but deliberate error');
+}
+
 echo $html['foot'];
+
