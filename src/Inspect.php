efaf827589c9883669527be351945eb89a11ea5f
jacobfriis <jacob.friis@simplecomplex.net>
2017-06-24 16:45:05 +0200

    documentation

--- src/Inspect.php
+++ src/Inspect.php
@@ -45,7 +45,7 @@ class Inspect
      */
     public static function getInstance(...$constructorParams)
     {
-        // Can't use null ternary ?? apparantly doesn't work for static prop.
+        // Unsure about null ternary ?? for class and instance vars.
         if (!static::$instance) {
             static::$instance = new static(...$constructorParams);
         }
